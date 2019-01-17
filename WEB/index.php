8aa13b48db0d338cdb9fb245e8e2b3b600311561
David Barrero <46119184+davidbarrero38@users.noreply.github.com>
2019-01-17 09:30:29 +0100

    Add files via upload

---
+++ WEB/index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116829806-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-116829806-1');
+</script>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+      <title>AutoGarden the automatic garden system!</title>
+  
+  </head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<header id="header">
+									<a href="index.html" class="logo"><strong>AutoGarden</strong> by David López Barrero</a>
+									<ul class="icons">
+										
+										<li><a href="https://www.facebook.com/AutoGarden-179396672782559/" class="icon fa-facebook"><span 														class="label">Facebook</span></a></li>
+										
+										<li><a href="https://www.instagram.com/autogarden_/" class="icon fa-instagram"><span 																					class="label">Instagram</span></a></li>
+										
+									</ul>
+								</header>
+
+							<!-- Banner -->
+								<section id="banner">
+									<div class="content">
+										<header>
+											<h1>Hi, welcome to AutoGarden<br />
+											</h1>
+                                          <br>
+                                         <br>
+										</header>
+										<p>Autogarden is an invention that will give you the opportunity to grow your own food anywhere in your house without having to worry about the care</p>
+										<ul class="actions"> <br>
+                                         <br> <br>
+                                         <br>
+											<li><a href="https://www.kickstarter.com/projects/1495756473/autogarden-the-automatic-garden-system?ref=user_menu" class="button big">Buy It</a></li>
+										</ul>
+									</div>
+									<span class="image object">
+										<img src="images/pic10.JPG" alt="" />
+									</span>
+								</section>
+                         
+									<section>
+                                      <header class="major">
+										<h2>Presentation video</h2>
+                                
+									</header>
+                                      <div class="posts">
+										<article>
+										  <iframe  src="https://www.youtube.com/embed/4YKCCkuvtW0" frameborder="0" 												allow="autoplay; encrypted-media" allowfullscreen></iframe>  
+											
+										</article>
+                                      <article>
+                                    
+                                         <h3>AutoGarden presentation video</h3> <p>As you can see in the video, the assembly is easy and so it is the installation. You can grow a large variety of plants in your home, no matter how much space you have, <b> AutoGarden</b> 							tries to bring people healthy food that can be grown from home.
+						<br>Make it healthy </p>
+                                        
+                                        	<a href="https://www.youtube.com/watch?v=4YKCCkuvtW0" class="button big">Check the channel</a>  
+                                        
+                                       
+                              
+									</div>
+                                      
+                                     </section>
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>What does it offer?</h2>
+									</header>
+									<div class="features">
+										<article>
+										    <img src="icocnos/Entypo_d83c(7)_128.png">
+											<div class="content">
+												<h3>Nature in your home</h3>
+												<p>With this invention you can be more in touch with nature and cultivate a great variety of foods for your own 															consumption!</p>
+											</div>
+										</article>
+										<article>
+											 <img src="icocnos/linecons_e020(0)_128.png">
+											<div class="content">
+											<div class="content">
+												<h3>Searching for investors</h3>
+												<p>We are looking for funding to carry out the project, we are a StartUp of young guys and we have started a funding 														campaign on Kikstarted.</p>
+											</div>
+										</article>
+										<article>
+											 <img src="icocnos/wpzoom_e689(0)_128.png">
+											<div class="content">
+											<div class="content">
+												<h3>Positive evaluation</h3>
+												<p>We received a high rating from the customers who have the product.</p>
+											</div>
+										</article>
+										<article>
+											 <img src="icocnos/linea_2cf(3)_128.png">
+											<div class="content">
+											<div class="content">
+												<h3>Purpose</h3>
+												<p>With this invention you can cultivate, without having to worry about the bad weather, of the whole world, that 															your life is simpler!</p>
+											</div>
+										</article>
+									</div>
+								</section>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>Features</h2>
+									</header>
+									<div class="posts">
+										<article>
+											<a href="#" class="image"><img src="images/pic10.jpg" alt="" /></a>
+											<h3> 1 cubic meter</h3>
+											<p>With this you could cultive big plants like tomato plant and more bigger!</p>
+											<ul class="actions">
+											
+											</ul>
+										</article>
+                                      <article>
+                                        <h2> The kit includes </h2>
+                                        <lu> <br>
+                                          <li> Motherboard with sensors </li> <br>
+                                            <li>Protective plastic </li>   <br>
+                                          <li> Low consume light bulb </li>    <br>
+                                          <li> Water Pump </li>    <br>
+                                          <li> Seed and sediment kit </li>  <br>
+                                           <li> Beginner instructions</li>  
+                                        
+                                        </lu>
+                                        
+                                        
+                                         <br> <br> <br> <br> <br>
+                                        <h1>    80$ </h1>
+                                        <br> <br> <br>
+                                        	<a href="https://www.kickstarter.com/projects/1495756473/autogarden-the-automatic-garden-system?ref=user_menu" class="button big">Learn more</a>  
+                                        
+                                       
+                              
+									</div>
+								</section>
+
+						</div>
+					</div>
+
+				<!-- Sidebar -->
+					<div id="sidebar">
+						<div class="inner">
+
+							<!-- Search -->
+								
+							<!-- Menu -->
+								<nav id="menu">
+									<header class="major">
+										<h2>Menu</h2>
+									</header>
+									<ul>
+										<li><a href="index.php">Homepage</a></li>
+										<li><a href="generic.html">More information</a></li>
+									<!--	<li><a href="elements.html">Elements</a></li> -->
+                                      <li><a href="http://autogarden-es.stackstaging.com">En español</a></li> 
+										<li>
+											<span class="opener">Generals questions</span>
+											<ul>
+												<li><a href="#">Montandolo</a></li>
+												<li><a href="#">Errores</a></li>
+												<li><a href="#"></a></li>
+												
+											</ul>
+										</li>
+										
+									</ul>
+								</nav>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>News</h2>
+									</header>
+									<div class="mini-posts">
+										<article>
+											<a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
+											<p>Heres is Simba with his new Autogarden.</p>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>
+											<p>PRIMER CRECIMIENTO!</p>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>
+											<p>Comida ecológica en la mesa!!</p>
+										</article>
+									</div>
+									
+								</section>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>Ponerse en contacto</h2>
+									</header>
+									<p>Podeis contactarnos ante cualquier duda que tengais estaremos encantados de atenderles.</p>
+									<ul class="contact">
+										<li class="fa-envelope-o"><a href="#">autogarden.tech@gmail.com</a></li>
+										<li class="fa-phone">+34 685303424</li>
+										<li class="fa-home">Francisco Quevedo 25 28110<br />
+										Madrid</li>
+									</ul>
+								</section>
+
+							<!-- Footer -->
+							
+						</div>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
